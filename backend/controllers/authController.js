86642ead2a28f2b100ddcf37f44a921f6586e323
Ariya <apanpratama1103@gmail.com>
2025-11-21 20:10:52 +0700

    add function logout in authController

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -86,3 +86,12 @@ const login = async (req, res) => {
     }
   }
 };
+
+const logout = async (req, res) => {
+  res.clearCookie("token", {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict",
+  });
+  res.status(200).json({ success: true, message: "Logged out successfully" });
+};
